504f243d4cc1350604a719cdd84609239d69acea
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 02:16:08 +0800

    FAN; getters and setters

--- PET/pet_class.py
+++ PET/pet_class.py
@@ -1,8 +1,27 @@
 # create pet class
 class Pet:
-    def __init__(self, name, age, type):
+    def __init__(self, name, age, animal_type):
         self.__name = name
         self.__age = age
-        self.__type = type
-# define methods (create setter and getter)
-# create attributes
+        self.__animal_type = animal_type
+    # define methods (create setter and getter)
+    # getters
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
+    
+    # setters
+    def set_name(self, name):
+        self.__name = name
+
+    def set_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+    
