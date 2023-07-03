674d2fd772fdad55f8382b51e1b68daf64fb621b
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 02:23:09 +0800

    FAN; import Pet class, ask user

--- PET/pet_main.py
+++ PET/pet_main.py
@@ -1,4 +1,10 @@
 # import pet class
+from pet_class import Pet
+
 # ask user for the pet's name, age, and type of pet
+pet_name = input("What's your pet's name? ")
+pet_age = input("How old is he/she? ")
+pet_type = input("What animal is it? ")
+
 # create object
 # display data
