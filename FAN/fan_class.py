5f9e98846c07eea99348e75c2bcf0e0f6faa416c
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 16:09:56 +0800

    FAN; getters and setters

--- FAN/fan_class.py
+++ FAN/fan_class.py
@@ -7,9 +7,29 @@ class Fan:
         self.__radius = radius
         self.__color = color
         self.__power = power
-# define methods
-# create private int data for speed
-# create private bool data for power(ON/OFF)
-# create private float data for radius
-# create private string data for color
-# create getters
+    # define methods
+    # setters
+    def set_speed(self, speed):
+        self.__speed = speed # create private int data for speed
+    
+    def set_status(self, power):
+        self.__power = bool(power) # create private bool data for power(ON/OFF)
+    
+    def set_radius(self, radius):
+        self.__radius = float(radius) # create private float data for radius
+    
+    def set_color(self, color):
+        self.__color = str(color) # create private string data for color
+
+    # create getters
+    def get_speed(self):
+        return self.__speed
+
+    def get_status(self):
+        return self.__power
+    
+    def get_radius(self):
+        return self.__radius
+    
+    def get_color(self):
+        return self.__color
