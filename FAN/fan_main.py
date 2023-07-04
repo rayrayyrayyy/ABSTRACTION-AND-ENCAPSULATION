3e3c4a05f2e79ff4681b9a5f3ae7574561a809c6
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 17:11:30 +0800

    FAN; import fan class, create objects

--- FAN/fan_main.py
+++ FAN/fan_main.py
@@ -1,3 +1,10 @@
 # import fan class
-# create objects
+from fan_class import Fan
+
+def test_fan(): # create function
+
+    # create objects
+    fan_1 = Fan()
+    fan_2 = Fan()
+
 # display object's properties
