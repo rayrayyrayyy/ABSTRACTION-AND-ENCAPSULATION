d9f66611d6f41ce569090764f8f47ad56d56b32d
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 18:29:49 +0800

    FAN; import Car class, create object

--- CAR/car_main.py
+++ CAR/car_main.py
@@ -1,4 +1,8 @@
 # import car class
+from car_class import Car
+
 # create object
+users_car = Car ()
+
 # call accelerate method five times, display data
 # call brake method five times, display data
