f5d882290deff542062fa5d2c295e4192460cef7
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 19:17:57 +0800

    try

--- CAR/car_main.py
+++ CAR/car_main.py
@@ -1,8 +1,14 @@
 # import car class
 from car_class import Car
 
+# ask user
+year_model = int(input("what is year model is your car? "))
+make = input("Make of your car? ")
+
 # create object
-users_car = Car ()
+users_car = Car(year_model, make)
 
 # call accelerate method five times, display data
 # call brake method five times, display data
+
+users_car.car_info()
