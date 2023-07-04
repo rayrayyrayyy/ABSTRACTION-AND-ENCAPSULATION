f68b35e4bc6e80b57f886ae05c05a16dfc302ec7
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-05 03:45:12 +0800

    PET design hehe

--- PET/pet_main.py
+++ PET/pet_main.py
@@ -16,6 +16,8 @@ user_pet = Pet(pet_name, pet_age, pet_type)
 pet_text = pyfiglet.figlet_format("My Pet ", font = 'small', width = 30, justify = 'center')
 
 # display data
+print("...processing data")
+time.sleep(2)
 print('\n\033[1;33m' + '==='*10 + '\033[0m')
 print(Style.BRIGHT + Fore.LIGHTMAGENTA_EX + pet_text + Style.RESET_ALL)
 print('\033[1;33m' + '==='*10 + '\033[0m')
