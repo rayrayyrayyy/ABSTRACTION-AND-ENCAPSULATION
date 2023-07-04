011e45b4dee9870235967cec9df96de297395866
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 15:46:45 +0800

    CAR; getters and setters

--- CAR/car_class.py
+++ CAR/car_class.py
@@ -1,7 +1,29 @@
 # create car class
 class Car:
-    def __init__ (self, model = 2023, make = "Mazda MX-5", speed = 0):
+    def __init__ (self, model, make, speed = 0):
         self.__year_model = model
         self.__make = make
         self.__speed = speed
-# define methods
+    # define methods
+    #getters
+    def get_year_model(self):
+        return self.__year_model
+
+    def get_make(self):
+        return self.__make
+    
+    def get_speed(self):
+        return self.__speed
+    
+    # setters
+    def set_year_model(self, model):
+        self.__year_model = model
+
+    def set_make(self, make):
+        self.__make = make
+
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5 
