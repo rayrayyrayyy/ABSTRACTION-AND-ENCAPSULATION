890ece6e78dd315b823dd88fc6e0e29867e1a034
rayrayyrayyy <allssndr.ray@gmail.com>
2023-07-04 17:42:41 +0800

    FAN; set properties

--- FAN/fan_main.py
+++ FAN/fan_main.py
@@ -7,4 +7,15 @@ def test_fan(): # create function
     fan_1 = Fan()
     fan_2 = Fan()
 
-# display object's properties
+    # set objects's properties
+    fan_1.set_speed(Fan.fast_speed)
+    fan_1.set_radius(10)
+    fan_1.set_color('Yellow')
+    fan_1.set_status(1)
+
+    fan_2.set_speed(Fan.medium_speed)
+    fan_2.set_radius(5)
+    fan_2.set_color('Blue')
+    fan_2.set_status(0)
+
+    # display object's properties
